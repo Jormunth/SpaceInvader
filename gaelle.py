a097e3bb734354616a15a9fb8df72ca1624f80ff
GaelleLeroux <gaelleleroux974@gmail.com>
2022-01-06 09:26:44 +0100

    collision 1 protection

--- gaelle.py
+++ gaelle.py
@@ -21,32 +21,47 @@ def CreationProjectile(event):
     PPX=POSX
     PPY=POSY
     projectile=Canevas.create_oval(POSX-TailleVaisseau+r,POSY-TailleVaisseau+r,POSX+TailleVaisseau-r,POSY+TailleVaisseau-r,fill='purple')
-    Canevas.after(20,Bouger,PPX,PPY,projectile,i)
+    Canevas.after(20,bouger,PPX,PPY,projectile,i)
 
 
-def Bouger(PPX,PPY,projectile,i):
+def bouger(PPX,PPY,projectile,i):
     global vitesse_projectile, r
-    if PPY-TailleVaisseau+r-15*i>-Hauteur-1000:
+    verif=collision_projectile(PPX,PPY,projectile,i)
+    if verif==True:
+        Canevas.delete(projectile)
+    elif PPY-TailleVaisseau+r-vitesse_projectile*i>0:
         Canevas.coords(projectile,PPX-TailleVaisseau+r,PPY-TailleVaisseau+r-vitesse_projectile*i,PPX+TailleVaisseau-r,PPY+TailleVaisseau-r-vitesse_projectile*i)
-        print(PPY-TailleVaisseau+r-15*i)
-        Canevas.after(100,Bouger,PPX,PPY,projectile,i+1)
+        Canevas.after(100,bouger,PPX,PPY,projectile,i+1)
     else:
         Canevas.delete(projectile)
-    
+
+
+def collision_projectile(PPX,PPY,projectile,i):
+    if PPX-TailleVaisseau+r>=positionx_protection+20 and PPX+TailleVaisseau-r<=TailleProtection*2+positionx_protection+20:
+        if PPY-TailleVaisseau+r-vitesse_projectile*i<TailleProtection*2+positiony_protection+20:
+            Canevas.delete(protection)
+            return(True)
+    return(False)
 
 mw=Tk()
 mw.title("Vaisseau")
 
-vitesse_projectile=3
+vitesse_projectile=5
 r=15
 Largeur=480
 Hauteur=320
 POSX=Largeur/2
 POSY=Hauteur/2
+positionx_protection=Largeur/2
+positiony_protection=Hauteur/2
 TailleVaisseau=20
+TailleProtection=15
 
 Canevas=Canvas(mw,width=Largeur,height=Hauteur,bg='white')
 
+liste_protection_x=[0,20,25,0,20,25]
+
+protection=Canevas.create_rectangle(POSX+20,POSY+20,TailleProtection*2+POSX+20,TailleProtection*2+POSY+20,fill='pink')
 
 Vaisseau=Canevas.create_rectangle(POSX,POSY,TailleVaisseau*2+POSX,TailleVaisseau*2+POSY,fill='maroon')
 Canevas.focus_set()
