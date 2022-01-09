feb26b0ef535f1310f89b2268a8d9b60eff5f88b
MathieuZem <mathieu.zeman@cpe.fr>
2022-01-09 18:13:43 +0100

    mégaPush

--- enemy.py
+++ enemy.py
@@ -1,23 +1,39 @@
 class Enemy:
-    def __init__(self, tag, posX, posY,DY):
+    def __init__(self, tag,vitesse,posX, posY,DY):
         self.tag=tag
         self.posX=posX
         self.posY=posY
+        self.vitesse=vitesse
         self.DY=DY
-        #self.Enemy=ZoneDeJeu.create_oval(X-Rayon, Y-Rayon, X+Rayon, Y+Rayon, fill="red", outline="black", width=5)
 
 
-    #def deplacer(self,posX,posY,DX,DY,Rayon,Largeur,Hauteur):
-    def deplacer(self, posX, posY):
-        self.posX = posX
-        self.posY = posY
-        print("Position actuelle", self.posX, " ", self.posY,)
+    def getTag(self):
+        return self.tag
+
+    def getPosX(self):
+        self.posX
+        return self.posX
+
+    def getPosY(self):
+        return self.posY
+
+    def getVitesse(self):
+        return self.vitesse
+
+    def getDY(self):
+        return self.DY
+
+    def deplacer(self, NewposX, NewposY):
+        self.posX = NewposX
+        self.posY = NewposY
+        """ print("Position actuelle", self.posX, " ", self.posY,) """
     
     def tirer(self):
         print("Je tire")
 
-
+""" 
 my_enemy = Enemy(100, 100, 200, None)
 my_enemy.deplacer(200, 300)
 my_enemy.deplacer(150, 125)
 my_enemy.tirer()
+ """
--- interface.py
+++ interface.py
@@ -1,14 +1,6 @@
 from tkinter import *
-import math,random
-from  import Enemy
-
-def deplacer(event):
-    touche=event.keysym  
-    if touche =='<RIGHT>':
-        my_enemy.deplacer(my_enemy.posX + 20, my_enemy.posY)
-    if touche =='<LEFT>':
-        my_enemy.deplacer(my_enemy.posX - 20, my_enemy.posY)
-     #mettre à jour l'image
+import random,math
+import enemy as e
 
 
 mw = Tk()
@@ -16,7 +8,7 @@ mw.title('Space Invader')
 mw['bg']='grey'
 mw.geometry('1900x1008+0+0')
 
-
+Terrain = PhotoImage(file = "image/terrainFond.png")
 
 FrameDroit = Frame(mw)
 FrameDroit.pack(side="right")
@@ -36,13 +28,113 @@ FrameGauche.pack(side="left")
 ZoneDeJeu = Canvas(FrameGauche, width=1600, height =950, bg="white")
 ZoneDeJeu.pack(padx=10, pady=10)
 
-my_enemy = Enemy(100, 100, 200, None)
+ZoneDeJeu.create_image(0,0,anchor=NW,image=Terrain)
 
 
-Enemy = ZoneDeJeu.create_oval(my_enemy, , fill="red", outline="black", width=5)
+LARGEUR=1600
+HAUTEUR = 300
+RAYON = 30
 
-Canevas.bind('<Left>', deplacer)
-Canevas.bind('<Right>',deplacer)
+vitesse = 10
+
+difficulty = 2
+tag = 0
+Placement = difficulty+1
+DY = 2*RAYON
+nbEnnemie = difficulty
+
+frameCntEnemie = 5
+imageEnnemie = [PhotoImage(file='image/Ninja/animation/runRight_2.gif',format = 'gif -index %i' %(i)) for i in range(frameCntEnemie)]
+
+frameCntProj = 2
+imageProj = [PhotoImage(file='image/Shuriken/Shuriken.gif',format = 'gif -index %i' %(i)) for i in range(frameCntProj)]
+
+
+myEnemyList=[]
+myEnemy = []
+
+while nbEnnemie >= 1:
+    
+    tag += 1
+    X = (LARGEUR/Placement)*tag
+    Y = HAUTEUR/2
+
+    myEnemyList.append(e.Enemy(tag, vitesse, X, Y, DY))
+    myEnemy.append(ZoneDeJeu.create_image(X,Y, image = imageEnnemie[0]))
+
+    nbEnnemie -= 1
+
+    XtremG = myEnemyList[0].getPosX()
+    XtremD = myEnemyList[-1].getPosX()
+
+
+
+def deplacementEnemy(myEnemyList,myEnemy,vitesse,DY,LARGEUR,difficulty):
+    
+    DX = vitesse
+    DY = 0
+
+    XtremG = myEnemyList[0].getPosX()
+    XtremD = myEnemyList[-1].getPosX()
+
+    XtremG += DX
+    XtremD += DX    
+
+
+    if XtremD + 20 > LARGEUR:
+        XtremD= 2*(LARGEUR) - XtremD
+        vitesse = -DX
+        DY = 60
+
+    if XtremG -20 < 0:
+        XtremG = -XtremG
+        vitesse = -DX
+        DY = 60
+
+    for i,val in enumerate(myEnemy):
+        ZoneDeJeu.move(myEnemy[i],DX,DY)
+        myEnemyList[i].deplacer(myEnemyList[i].getPosX()+DX,myEnemyList[i].getPosY()+DY)
+
+
+    FrameGauche.after(20,deplacementEnemy,myEnemyList,myEnemy,vitesse,DY,LARGEUR,difficulty)
+
+
+difficulty
+
+
+ProjectileEnemy = []
+
+def autoTir(difficulty,ProjectileEnemy):
+
+    max = len(myEnemy)-1
+
+    rand=random.randint(0,max)
+
+    coordsEnemyX = myEnemyList[rand].getPosX()
+    coordsEnemyY = myEnemyList[rand].getPosY()
+    ProjectileEnemylast=ZoneDeJeu.create_image(coordsEnemyX,coordsEnemyY, image = imageProj[0])
+    """ ProjectileEnemylast=ZoneDeJeu.create_line(coordsEnemyX, coordsEnemyY, coordsEnemyX, coordsEnemyY+30, width=6) """
+    ProjectileEnemy.append(ProjectileEnemylast)
+
+    FrameGauche.after(1500,autoTir,difficulty,ProjectileEnemy)
+    deplacementProjectEautoTir(ProjectileEnemylast,coordsEnemyY)
+
+
+def deplacementProjectEautoTir(ProjectileEnemylast,coordsEnemyY):
+
+    coordsEnemyY += 30
+    ZoneDeJeu.move(ProjectileEnemylast, 0, 20)
+    
+    FrameGauche.after(20,deplacementProjectEautoTir,ProjectileEnemylast,coordsEnemyY)
+
+
+
+deplacementEnemy(myEnemyList,myEnemy,vitesse,DY,LARGEUR,difficulty)
+autoTir(difficulty,ProjectileEnemy)
+
+
+
+
 
 
 
